b04a482b0c11721f9bbc049749477b7260a76d84
sawanoboly <sawanoboriyu@higanworks.com>
2015-08-12 18:09:00 +0900

    rename spec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'mandrill/template/manager'
+require 'mandrill_template/cli'
+require 'mandrill_template/template'
+require 'thor/group'
